11a061481c673f23c0bc6edfc8453ce408e318aa
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-03-07 00:03:56 +0530

    Assignments

---
+++ CJS/D26/script.js
@@ -0,0 +1,34 @@
+// 1
+console.log("Conncected");
+
+//* alert("hi");
+
+//? let nam = prompt("Your good Name");
+//? console.log(`${nam}, Welcome to cohort`);
+
+//! let city= "Bhopal";
+//!  console.log(city)
+
+//* var a = "Harsh";
+//* let b = "Sheryians";
+//* const c = "“Schoolˮ"
+//* a="Harsh Sharma";
+//* b= "Harkirat singh"
+//* c= "college"
+//* {
+//*     let num=20;
+//* }
+//* console.log(num);
+
+//? nam = "xyz";
+//? age = 12;
+//? city = "abc";
+//? console.log(`${nam}: Name \n ${age}:Age \n ${city}:city`);
+//? console.info(`${nam}: Name \n ${age}:Age \n ${city}:city`);
+//? console.warn(`${nam}: Name \n ${age}:Age \n ${city}:city`);
+
+let msg="I love  Development";
+console.log(msg.slice(2,6)); 
+console.log(msg.split(" "));
+console.log(msg.replace("love","❤️") );
+console.log(msg);
---
+++ CJS/script.js
@@ -0,0 +1,4 @@
+console.log("javascript Cohort Assignments");
+ const nam=prompt("Hey Welcome to Assignments page\nEnter Your Name");
+ alert(`Welcome to Cohort Assignments ${nam}`);
+
