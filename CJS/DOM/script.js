d3d136ea38299db110b04da5f034b64fc5cfbba4
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-03-11 02:06:11 +0530

    Counter

--- CJS/DOM/script.js
+++
@@ -1,9 +0,0 @@
-var h1 = document.querySelector('h1')
-var btn =  document.querySelector('button')
-
-btn.addEventListener('click',function(){
-    h1.innerHTML = 'Hi Bye Hello'
-    h1.style.color = 'red'
-    h1.style.fontSize =  '60px'
-    btn.innerHTML = "Click Me 😁"
-})
