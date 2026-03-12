3c637046576f646de080c03aae08a3a94e96d766
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-03-13 00:10:30 +0530

    Random color code with copy feature

--- S_Random_color_generator/script.js
+++ S_Random_color_generator/script.js
@@ -1,18 +1,32 @@
-let r = Math.trunc((Math.random() * 255));
-let g = Math.trunc((Math.random() * 255));
-let b = Math.trunc((Math.random() * 255));
 
+let color_value="";
 var color = document.querySelector('.color');
-var p = document.querySelector('p');
-var button = document.querySelector('button');
+var p = document.querySelector('.text');
+var button = document.querySelector('#change');
+const value = document.querySelector('.value');
+const copyBtn = document.querySelector('#copy');
+
+copyBtn.addEventListener('click', function() {
+    navigator.clipboard.writeText(color_value).then(() => {
+        alert("Color Copied: " + color_value);
+    })
+        .catch(err => {
+            console.error("Failed to Copy", err);
+        });
+});
 
 button.addEventListener('click', function () {
-    let r = Math.trunc((Math.random() * 255));
+    let r = Math.trunc((Math.random() * 255)); 
     let g = Math.trunc((Math.random() * 255));
     let b = Math.trunc((Math.random() * 255));
-    color.style.backgroundColor = `rgb(${r},${g},${b})`;
-    console.log(`rgb(${r},${g},${b})`);
-    p.style.display="none"
+
+    color_value = `rgb(${r},${g},${b})`;
+    color.style.backgroundColor = color_value;
+
+    value.innerHTML = color_value
+    console.log(color_value);
+
+    p.style.display = "none"
 })
 
 
