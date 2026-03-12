0e6712d1ab92ac9142be062ef03f0b70718257b9
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-03-12 17:50:48 +0530

    Mobile touch gesture updated

--- Insta_feature/script.js
+++ Insta_feature/script.js
@@ -1,14 +1,30 @@
 var main = document.querySelector(".main");
 var love = document.querySelector("i");
 
-main.addEventListener("dblclick", function () {
+//desktop mode behavior
+main.addEventListener("dblclick", showLove);
+
+function showLove() {
     love.style.transform = "translate(-50%,-50%) scale(1)";
     love.style.color = "#f9c3b3"
-    love.style.opacity="0.9"
+    love.style.opacity = "0.9"
     setTimeout(function () {
-        love.style.opacity="0";
+        love.style.opacity = "0";
     }, 500)
     setTimeout(function () {
         love.style.transform = "translate(-50%,-50%) scale(0)";
     }, 1000)
-})
+}
+
+// mobile mode behavior
+
+let lastTap = 0;
+main.addEventListener("touchend", function (e) {
+    let currentTime = new Date().getTime();
+    let tapLength = currentTime - lastTap;
+    if (tapLength < 300 && tapLength > 0) {
+        showLove();
+        e.preventDefault();
+    }
+    lastTap = currentTime;
+});
