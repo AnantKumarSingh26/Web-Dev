f2794d3f279beede7d9addeff19811c361892e5b
AnantKumarSingh26 <anantsingh.code@gmail.com>
2026-03-31 19:21:55 +0530

    Finish staging before rebase

--- D_63_QS/script.js
+++ D_63_QS/script.js
@@ -1,5 +1,5 @@
 const api='1616f68b69c9fa4dad2db19cb5ea3e5f1616f68b69c9fa4dad2db19cb5ea3e5f'
-const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${api}`;
+const url = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${api}`;
 
 async function  getWeather(city = 'London') {
     let raw = await fetch(url);
@@ -8,4 +8,10 @@ async function  getWeather(city = 'London') {
     let realData = await raw.json();
     console.log(realData);
 }
-getWeather();
+try{
+    getWeather();
+
+}catch(err){
+    console.log('We are having error');
+    
+}
